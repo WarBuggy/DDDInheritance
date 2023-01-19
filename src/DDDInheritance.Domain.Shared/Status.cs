73782a843790c824d9100c7329913eb8bcd518ad
WarBuggy <hovanbuu@gmail.com>
2023-01-19 13:13:42 +0700

    * Add Status enum

---
+++ src/DDDInheritance.Domain.Shared/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDInheritance
+{
+    public enum Status
+    {
+        NEW = 0,
+        OPEN = 1,
+        INPROGRESS = 2,
+        CLOSED = 3,
+        CANCELLED = 4,
+    }
+}
