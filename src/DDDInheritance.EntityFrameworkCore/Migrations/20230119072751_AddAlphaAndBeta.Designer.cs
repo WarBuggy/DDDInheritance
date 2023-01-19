d1759eb77694957c9a724ccfceb4476f2d13ae70
WarBuggy <hovanbuu@gmail.com>
2023-01-19 14:29:43 +0700

    * Create migration for Alpha and Beta

--- src/DDDInheritance.EntityFrameworkCore/Migrations/20230119072751_AddAlphaAndBeta.Designer.cs
+++ src/DDDInheritance.EntityFrameworkCore/Migrations/20230119072751_AddAlphaAndBeta.Designer.cs
@@ -4,6 +4,7 @@ using DDDInheritance.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace DDDInheritance.Migrations
 {
     [DbContext(typeof(DDDInheritanceDbContext))]
-    partial class DDDInheritanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230119072751_AddAlphaAndBeta")]
+    partial class AddAlphaAndBeta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
