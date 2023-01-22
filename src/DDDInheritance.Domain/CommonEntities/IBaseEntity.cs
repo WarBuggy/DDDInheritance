d2bbf61d6ec9a5bf9679931ed4e7829765b7c60a
WarBuggy <hovanbuu@gmail.com>
2023-01-22 15:44:44 +0700

    * Forgot to add IBaseEntity to the previous commit
    * Modify IBaseEntity to implement IMultiTenant, IAggregateRoot<Guid>, IHasConcurrencyStamp, in addition to IEntity<Guid>
    
    * Add method void SetId(Guid id); to IBaseEntity interface

--- src/DDDInheritance.Domain/CommonEntities/IBaseEntity.cs
+++ src/DDDInheritance.Domain/CommonEntities/IBaseEntity.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Volo.Abp.Domain.Entities;
+using Volo.Abp.MultiTenancy;
 
 namespace DDDInheritance.CommonEntities
 {
-    public interface IBaseEntity : IEntity<Guid>
+    public interface IBaseEntity : IMultiTenant, IEntity<Guid>, IAggregateRoot<Guid>, IHasConcurrencyStamp
     {
         string Code { get; set; }
 
@@ -12,5 +13,7 @@ namespace DDDInheritance.CommonEntities
         Status? Status { get; set; }
 
         Guid? Linked { get; set; }
+
+       void SetId(Guid id);
     }
 }
