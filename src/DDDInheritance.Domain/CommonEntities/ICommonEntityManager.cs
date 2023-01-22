5f6e3755362350ba3178ca0f7238f34303424162
WarBuggy <hovanbuu@gmail.com>
2023-01-22 23:14:33 +0700

    * Add ICommonEntityManager

---
+++ src/DDDInheritance.Domain/CommonEntities/ICommonEntityManager.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
+using Volo.Abp;
+
+namespace DDDInheritance.CommonEntities
+{
+    public interface ICommonEntityManager<T, TRepository> 
+    where T : class, IBaseEntity
+    where TRepository : ICommonEntityRepository<T>
+    {
+        Task<T> CreateAsync(string code, string name, Status? status = null, Guid? linked = null);
+
+        Task<T> CreateAsync(T baseEntity);
+
+        Task<T> UpdateAsync(Guid id,
+            string code, string name, Status? status = null, Guid? linked = null,
+            [CanBeNull] string concurrencyStamp = null);
+    }
+}
