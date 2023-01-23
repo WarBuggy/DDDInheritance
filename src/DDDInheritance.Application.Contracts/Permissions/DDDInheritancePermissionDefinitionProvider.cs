4fc5d84a3f93e056882f27b8e2090a452692f792
WarBuggy <hovanbuu@gmail.com>
2023-01-23 13:42:49 +0700

    * Add CRUD permissions for Alpha and Beta

--- src/DDDInheritance.Application.Contracts/Permissions/DDDInheritancePermissionDefinitionProvider.cs
+++ src/DDDInheritance.Application.Contracts/Permissions/DDDInheritancePermissionDefinitionProvider.cs
@@ -16,6 +16,15 @@ public class DDDInheritancePermissionDefinitionProvider : PermissionDefinitionPr
 
         //Define your own permissions here. Example:
         //myGroup.AddPermission(DDDInheritancePermissions.MyPermission1, L("Permission:MyPermission1"));
+        var alphaPermission = myGroup.AddPermission(DDDInheritancePermissions.Alphas.Default, L("Permission:DDDInheritance:Alphas"));
+        alphaPermission.AddChild(DDDInheritancePermissions.Alphas.Create, L("Permission:DDDInheritance:Create"));
+        alphaPermission.AddChild(DDDInheritancePermissions.Alphas.Edit, L("Permission:DDDInheritance:Edit"));
+        alphaPermission.AddChild(DDDInheritancePermissions.Alphas.Delete, L("Permission:DDDInheritance:Delete"));
+
+        var betaPermission = myGroup.AddPermission(DDDInheritancePermissions.Betas.Default, L("Permission:DDDInheritance:Betas"));
+        betaPermission.AddChild(DDDInheritancePermissions.Betas.Create, L("Permission:DDDInheritance:Create"));
+        betaPermission.AddChild(DDDInheritancePermissions.Betas.Edit, L("Permission:DDDInheritance:Edit"));
+        betaPermission.AddChild(DDDInheritancePermissions.Betas.Delete, L("Permission:DDDInheritance:Delete"));
     }
 
     private static LocalizableString L(string name)
--- src/DDDInheritance.Application.Contracts/Permissions/DDDInheritancePermissions.cs
+++ src/DDDInheritance.Application.Contracts/Permissions/DDDInheritancePermissions.cs
@@ -1,3 +1,6 @@
+using DDDInheritance.Alphas;
+using DDDInheritance.Betas;
+
 namespace DDDInheritance.Permissions;
 
 public static class DDDInheritancePermissions
@@ -13,4 +16,20 @@ public static class DDDInheritancePermissions
 
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class Alphas
+    {
+        public const string Default = GroupName + ".Alphas";
+        public const string Edit = Default + ".Edit";
+        public const string Create = Default + ".Create";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class Betas
+    {
+        public const string Default = GroupName + ".Betas";
+        public const string Edit = Default + ".Edit";
+        public const string Create = Default + ".Create";
+        public const string Delete = Default + ".Delete";
+    }
 }
+
