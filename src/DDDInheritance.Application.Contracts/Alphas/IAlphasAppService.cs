e5ed555fd5388cbd58a2eedd21b734aac0bde020
WarBuggy <hovanbuu@gmail.com>
2023-01-23 11:47:08 +0700

    * Add method Task<string> GetStatus(Guid id); to Alpha app service

--- src/DDDInheritance.Application.Contracts/Alphas/IAlphasAppService.cs
+++ src/DDDInheritance.Application.Contracts/Alphas/IAlphasAppService.cs
@@ -1,8 +1,11 @@
 ﻿using DDDInheritance.CommonEntities;
+using System.Threading.Tasks;
+using System;
 
 namespace DDDInheritance.Alphas
 {
     public interface IAlphasAppService : ICommonEntitiesAppService
     {
+        Task<string> GetStatus(Guid id);
     }
 }
--- src/DDDInheritance.Application/Alphas/AlphasAppService.cs
+++ src/DDDInheritance.Application/Alphas/AlphasAppService.cs
@@ -1,4 +1,6 @@
 ﻿using DDDInheritance.CommonEntities;
+using DDDInheritance.Localization;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +9,28 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Caching;
+using Volo.Abp.Domain.Repositories;
 
 namespace DDDInheritance.Alphas
 {
     public class AlphasAppService : CommonEntitiesAppService<Alpha, IAlphaManager>, IAlphasAppService
     {
+        private readonly IStringLocalizer<DDDInheritanceResource> _localizer;
+
         public AlphasAppService(IAlphaRepository repository,
             IAlphaManager manager,
-            IDistributedCache<CommonEntityExcelDownloadTokenCacheItem, string> excelDownloadTokenCache) 
-            :base(repository, manager, excelDownloadTokenCache)
+            IDistributedCache<CommonEntityExcelDownloadTokenCacheItem, string> excelDownloadTokenCache,
+            IStringLocalizer<DDDInheritanceResource> localizer)
+            : base(repository, manager, excelDownloadTokenCache)
         {
+            _localizer = localizer;
+        }
+
+        public async Task<string> GetStatus(Guid id)
+        {
+            Status? status = await ((IAlphaRepository)_repository).GetStatus(id);
+            string statusString = status == null ? "NULL" : status.ToString();
+            return _localizer[$"DDDInheritance:EnumValue:Status:{statusString}"];
         }
     }
 }
