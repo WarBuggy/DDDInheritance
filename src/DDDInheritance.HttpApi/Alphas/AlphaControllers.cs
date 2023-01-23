05366fc5a12c289a21d78a188fd0b5bb01b5c5e5
WarBuggy <hovanbuu@gmail.com>
2023-01-23 10:56:01 +0700

    * Add controllers for CommonEntity and Alpha

---
+++ src/DDDInheritance.HttpApi/Alphas/AlphaControllers.cs
@@ -0,0 +1,17 @@
+﻿using DDDInheritance.CommonEntities;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+
+namespace DDDInheritance.Alphas
+{
+    [RemoteService]
+    [Area("app")]
+    [ControllerName("Alpha")]
+    [Route("api/app/alphas")]
+    public class AlphaControllers : CommonControllers, IAlphasAppService
+    {
+        public AlphaControllers(IAlphasAppService appService) : base(appService) 
+        {
+        }
+    }
+}
---
+++ src/DDDInheritance.HttpApi/CommonEntities/CommonEntityControllers.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Content;
+using Volo.Abp.Gdpr;
+using Volo.Abp;
+
+namespace DDDInheritance.CommonEntities
+{
+
+    [RemoteService]
+    [Area("app")]
+    [ControllerName("Common")]
+    [Route("api/app/commons")]
+    public class CommonControllers : AbpController, ICommonEntitiesAppService
+    {
+        private readonly ICommonEntitiesAppService _appService;
+
+        public CommonControllers(ICommonEntitiesAppService appService)
+        {
+            _appService = appService;
+        }
+
+        [HttpGet]
+        public virtual Task<PagedResultDto<CommonEntityDto>> GetListAsync(GetCommonEntitiesInput input)
+        {
+            return _appService.GetListAsync(input);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public virtual Task<CommonEntityDto> GetAsync(Guid id)
+        {
+            return _appService.GetAsync(id);
+        }
+
+        [HttpPost]
+        public virtual Task<CommonEntityDto> CreateAsync(CommonEntityCreateDto input)
+        {
+            return _appService.CreateAsync(input);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public virtual Task<CommonEntityDto> UpdateAsync(Guid id, CommonEntityUpdateDto input)
+        {
+            return _appService.UpdateAsync(id, input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public virtual Task DeleteAsync(Guid id)
+        {
+            return _appService.DeleteAsync(id);
+        }
+
+        [HttpGet]
+        [Route("as-excel-file")]
+        public virtual Task<IRemoteStreamContent> GetListAsExcelFileAsync(CommonEntityExcelDownloadDto input)
+        {
+            return _appService.GetListAsExcelFileAsync(input);
+        }
+
+        [HttpGet]
+        [Route("download-token")]
+        public Task<DownloadTokenResultDto> GetDownloadTokenAsync()
+        {
+            return _appService.GetDownloadTokenAsync();
+        }
+    }
+}
