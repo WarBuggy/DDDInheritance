e1e1c6b6aed0fb2b53ee7ce3e9bde3a9d9d04c48
WarBuggy <hovanbuu@gmail.com>
2023-01-23 12:22:36 +0700

    * Remove unused imports

--- src/DDDInheritance.Domain/Alphas/IAlphaRepository.cs
+++ src/DDDInheritance.Domain/Alphas/IAlphaRepository.cs
@@ -1,8 +1,5 @@
 ﻿using DDDInheritance.CommonEntities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DDDInheritance.Alphas
